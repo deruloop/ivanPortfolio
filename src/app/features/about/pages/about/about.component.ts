420e337052b991414954c24fa096721a8d811811
Cristiano Calicchia <c.calicchia@icloud.com>
2025-01-08 18:39:27 +0100

    feat: about page completed

--- src/app/features/about/pages/about/about.component.ts
+++ src/app/features/about/pages/about/about.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, Inject, Renderer2, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-about-page',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutPageComponent {}
+export class AboutPageComponent {
+  constructor(
+    private route: ActivatedRoute, 
+    private renderer: Renderer2,
+    @Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      // This block runs only in the browser
+      this.renderer.addClass(document.body, 'about');
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      // Cleanup when the component is destroyed
+      this.renderer.removeClass(document.body, 'about');
+    }
+  }
+}
